7bdbaa239fa1fe528a8d93875585f883de38829c
ribatturan <ahmetribat.turan@stu.fsm.edu.tr>
2020-07-27 00:09:50 +0300

    Tasarımın bir bölümü eklendi.

---
+++ deneme/deneme/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.dataSaveBtn = new System.Windows.Forms.Button();
+            this.getRefBtn = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.analyzeBtn = new System.Windows.Forms.Button();
+            this.plotexportBtn = new System.Windows.Forms.Button();
+            this.newmsrmntBtn = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Smart Food Analyzer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Device Connection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(14, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Enter or Select Food Type";
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Location = new System.Drawing.Point(160, 73);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(125, 21);
+            this.comboBoxDevice.TabIndex = 15;
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.disconnectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.disconnectBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.disconnectBtn.Location = new System.Drawing.Point(18, 122);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(109, 32);
+            this.disconnectBtn.TabIndex = 16;
+            this.disconnectBtn.Text = "Disconnect";
+            this.disconnectBtn.UseVisualStyleBackColor = false;
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.connectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.connectBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.connectBtn.Location = new System.Drawing.Point(133, 122);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(152, 32);
+            this.connectBtn.TabIndex = 17;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = false;
+            // 
+            // dataSaveBtn
+            // 
+            this.dataSaveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.dataSaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dataSaveBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataSaveBtn.Location = new System.Drawing.Point(18, 169);
+            this.dataSaveBtn.Name = "dataSaveBtn";
+            this.dataSaveBtn.Size = new System.Drawing.Size(109, 34);
+            this.dataSaveBtn.TabIndex = 18;
+            this.dataSaveBtn.Text = "Save Data";
+            this.dataSaveBtn.UseVisualStyleBackColor = false;
+            // 
+            // getRefBtn
+            // 
+            this.getRefBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.getRefBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.getRefBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.getRefBtn.Location = new System.Drawing.Point(133, 169);
+            this.getRefBtn.Name = "getRefBtn";
+            this.getRefBtn.Size = new System.Drawing.Size(152, 34);
+            this.getRefBtn.TabIndex = 19;
+            this.getRefBtn.Text = "Get Reference";
+            this.getRefBtn.UseVisualStyleBackColor = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(18, 288);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // analyzeBtn
+            // 
+            this.analyzeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.analyzeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.analyzeBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.analyzeBtn.Location = new System.Drawing.Point(174, 288);
+            this.analyzeBtn.Name = "analyzeBtn";
+            this.analyzeBtn.Size = new System.Drawing.Size(111, 39);
+            this.analyzeBtn.TabIndex = 22;
+            this.analyzeBtn.Text = "Analyze";
+            this.analyzeBtn.UseVisualStyleBackColor = false;
+            // 
+            // plotexportBtn
+            // 
+            this.plotexportBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.plotexportBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.plotexportBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.plotexportBtn.Location = new System.Drawing.Point(62, 386);
+            this.plotexportBtn.Name = "plotexportBtn";
+            this.plotexportBtn.Size = new System.Drawing.Size(162, 43);
+            this.plotexportBtn.TabIndex = 23;
+            this.plotexportBtn.Text = "Plot and Export";
+            this.plotexportBtn.UseVisualStyleBackColor = false;
+            // 
+            // newmsrmntBtn
+            // 
+            this.newmsrmntBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.newmsrmntBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.newmsrmntBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.newmsrmntBtn.Location = new System.Drawing.Point(62, 448);
+            this.newmsrmntBtn.Name = "newmsrmntBtn";
+            this.newmsrmntBtn.Size = new System.Drawing.Size(162, 49);
+            this.newmsrmntBtn.TabIndex = 24;
+            this.newmsrmntBtn.Text = "New Measurement";
+            this.newmsrmntBtn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::deneme.Properties.Resources.cizgi3;
+            this.pictureBox4.Location = new System.Drawing.Point(306, 48);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1, 477);
+            this.pictureBox4.TabIndex = 27;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::deneme.Properties.Resources.cizgi2;
+            this.pictureBox3.Location = new System.Drawing.Point(5, 361);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(295, 10);
+            this.pictureBox3.TabIndex = 26;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::deneme.Properties.Resources.cizgi2;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 218);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(295, 10);
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Image = global::deneme.Properties.Resources.minimize;
+            this.minimizeBtn.Location = new System.Drawing.Point(902, 7);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(32, 32);
+            this.minimizeBtn.TabIndex = 11;
+            this.minimizeBtn.TabStop = false;
+            this.minimizeBtn.UseVisualStyleBackColor = true;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseBtn.FlatAppearance.BorderSize = 0;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.Image = global::deneme.Properties.Resources.close;
+            this.CloseBtn.Location = new System.Drawing.Point(952, 7);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(32, 32);
+            this.CloseBtn.TabIndex = 10;
+            this.CloseBtn.TabStop = false;
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::deneme.Properties.Resources.cizgi1;
+            this.pictureBox1.Location = new System.Drawing.Point(-28, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(3154, 2);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(995, 556);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.newmsrmntBtn);
+            this.Controls.Add(this.plotexportBtn);
+            this.Controls.Add(this.analyzeBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.getRefBtn);
+            this.Controls.Add(this.dataSaveBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.disconnectBtn);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.minimizeBtn);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxDevice;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button dataSaveBtn;
+        private System.Windows.Forms.Button getRefBtn;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button analyzeBtn;
+        private System.Windows.Forms.Button plotexportBtn;
+        private System.Windows.Forms.Button newmsrmntBtn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
